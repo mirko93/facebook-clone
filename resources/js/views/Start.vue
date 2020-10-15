9d4bd31160e91d7260f685e89bcc4a55e727dc99
Mirko Skopljak <mirkoskopljak@pop-os.localdomain>
2020-10-15 17:41:35 +0200

    first commit - project ready to go...

---
+++ resources/js/views/Start.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        dsadsadsad
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Start",
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
