9c2de01d2d3988aa9014d098f447631f71698a38
Mirko Skopljak <mirkoskopljak@pop-os.localdomain>
2020-10-16 14:06:15 +0200

    Create PHPUNIT PostToTimeLineTest (user can post a text post)

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Resources\Post as PostResource;
+
+class PostController extends Controller
+{
+    public function store()
+    {
+        $data = request()->validate([
+            'data.attributes.body' => '',
+        ]);
+
+        $post = request()->user()->posts()->create($data['data']['attributes']);
+
+        return new PostResource($post);
+    }
+}
---
+++ tests/Feature/PostToTimelineTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostToTimelineTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_post_a_text_post()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs($user = User::factory()->create(), 'api');
+
+        $response = $this->post('/api/posts', [
+            'data' => [
+                'type' => 'posts',
+                'attributes' => [
+                    'body' => 'Testing body',
+                ]
+            ]
+        ]);
+
+        $post = Post::first();
+
+        $this->assertCount(1, Post::all());
+        $this->assertEquals($user->id, $post->user_id);
+        $this->assertEquals('Testing body', $post->body);
+
+        $response->assertStatus(201)
+                ->assertJson([
+                    'data' => [
+                        'type' => 'posts',
+                        'post_id' => $post->id,
+                        'attributes' => [
+                            'posted_by' => [
+                                'data' => [
+                                    'attributes' => [
+                                        'name' => $user->name,
+                                    ]
+                                ]
+                            ],
+                            'body' => 'Testing body',
+                        ]
+                    ],
+
+                    'links' => [
+                        'self' => url('/posts/'.$post->id),
+                    ]
+                ]);
+    }
+}
