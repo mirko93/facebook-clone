8a88238f185ccf4ec1693c7093acdb2adc3f6d16
mirko93 <skopljak.mirko93@gmail.com>
2020-10-19 16:45:39 +0200

    FrontEnd Vue (Nav, Sidebar, Post, NewPost, NewsFeed)

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
-        Start
-        <router-view></router-view>
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav />
+
+        <div class="flex flex-1 overflow-y-hidden">
+            <Sidebar />
+
+            <div class="w-2/3 overflow-x-hidden">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import Nav from './Nav';
+import Sidebar from './Sidebar';
+
     export default {
         name: "App",
+
+        components: {
+            Nav,
+            Sidebar,
+        }
     }
 </script>
 
