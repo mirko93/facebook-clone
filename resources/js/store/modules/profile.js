ccb0247ddcb60450e3bb02fdecabb8b172ee0a9d
mirko93 <skopljak.mirko93@gmail.com>
2020-11-04 14:07:05 +0100

    fix bugs

--- resources/js/store/modules/profile.js
+++ resources/js/store/modules/profile.js
@@ -22,10 +22,11 @@ const getters = {
         return state.user.data.attributes.friendship;
     },
     friendButtonText: (state, getters, rootState) => {
-        if (getters.friendship === null) {
+        if (rootState.User.user.data.user_id === state.user.data.user_id) {
+            return '';
+        } else if (getters.friendship === null) {
             return 'Add Friend';
-        } else if (getters.friendship.data.attributes.confirmed_at === null 
-            && getters.friendship.data.attributes.friend_id !== rootState.User.user.data.user_id) {
+        } else if (getters.friendship.data.attributes.confirmed_at === null && getters.friendship.data.attributes.friend_id !== rootState.User.user.data.user_id) {
             return 'Pending Friend Request';
         } else if (getters.friendship.data.attributes.confirmed_at !== null) {
             return '';
@@ -60,7 +61,11 @@ const actions = {
                 commit('setPostsStatus', 'error');
             });
     },
-    sendFriendRequest({commit, state}, friendId) {
+    sendFriendRequest({commit, getters}, friendId) {
+        if (getters.friendButtonText !== 'Add Friend') {
+            return;
+        }
+
         axios.post('/api/friend-request', { 'friend_id': friendId })
             .then(res => {
                 commit('setUserFriendship', res.data);
--- tests/Feature/FriendsTest.php
+++ tests/Feature/FriendsTest.php
@@ -45,6 +45,25 @@ class FriendsTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_send_a_friend_request_only_once()
+    {
+        $this->actingAs($user = User::factory()->create(), 'api');
+        $anotherUser = User::factory()->create();
+
+        $this->post('/api/friend-request', [
+            'friend_id' => $anotherUser->id,
+        ])->assertStatus(200);
+
+        $this->post('/api/friend-request', [
+            'friend_id' => $anotherUser->id,
+        ])->assertStatus(200);
+
+        $friendRequest = Friend::all();
+
+        $this->assertCount(1, $friendRequest);
+    }
+
+    /** @test */
     public function only_valid_users_can_be_friend_requested()
     {
         $this->actingAs($user = User::factory()->create(), 'api');
